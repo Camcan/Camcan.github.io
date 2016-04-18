f1df4ddb68cdf12ef488f94dc97bb9e9aa2af3d2
Camcan <mr.campbell.hawkes@gmail.com>
2016-04-18 20:03:05 +1200

    Async loading + reavealing of fragments w/ click

--- js/app.js
+++ js/app.js
@@ -10,7 +10,6 @@ $(document).ready(function(){
   var text = ""
 
   function typeText(elementId, string, start, callback) {
-      var typeText = arguments.callee
     if (string) {
 
       console.log("Run: " + start)
@@ -23,125 +22,132 @@ $(document).ready(function(){
       var char = string.slice(-1)
       
       if( char === text.slice(-1)) {
-        console.log("I'm False")
         isTag = false
       }
-      if (isTag === false) return callback()
+      if (isTag === false) {
+        console.log("FALSY")
+        return callback()
+      }
 
       setTimeout(typeText, 140, elementId, string, start, callback)
     }
   }
-  
-  typeText("typed-title", title, 0,
-    function(){
-      $("#return").show()
-      $("#content").fadeIn()
-      console.log("done")
-    }
-  )()
 
-  // for(var key in sections){
-  //   loadSection(sections[key])
-  // }
+  var showAll = function(){
+      $('#return').show()
+      $('#content').fadeIn()
+  }
 
-  // var sections = {
-  //   RoR: {
-  //     display: false,
-  //     name: "RoR-portfolio",
-  //     items: [
-  //       [],
-  //       [],
-  //       []
-  //     ],
-  //     details: {},
-  //   },
-  //   js: {
-  //     display: false,
-  //     name: "js-portfolio",
-  //     items: [
-  //       {
-  //         id: "first-photo",
-  //         background: "image "
+  typeText('typed-title', title, 0,
+   showAll
+  )
 
-  //       },
-  //       {
-  //         id: "second-photo",
-  //         // background: 
-  //       },
+  for(var key in sections){
+    loadSection(sections[key])
+  }
+
+  var sections = {
+    RoR: {
+      display: false,
+      name: 'RoR-portfolio',
+      items: [
+        [],
+        [],
+        []
+      ],
+      details: {},
+    },
+    js: {
+      display: false,
+      name: 'js-portfolio',
+      items: [
+        {
+          id: 'first-photo',
+          background: "image "
+
+        },
+        {
+          id: 'second-photo',
+          // background: 
+        },
         
-  //     ],
-  //     details: {},
-  //   }
-  // }
+      ],
+      details: {},
+    }
+  }
 
-  // var addNewClass = function(elementId, newClass) {
-  //   $("#"+elementId).addClass(newClass)
-  // }
+  var addNewClass = function(elementId, newClass) {
+    $('#'+elementId).addClass(newClass)
+  }
 
  
-  // var loadTemplate = function(template, details) {
-  //   $.get((template.name + '.html'), function(temp) {
-  //     var htmlString = Mustache.render(temp, details);
-  //     $('#'+ template.name).html(htmlString);
-  //   })
-  // }
+  var loadTemplate = function(template, details) {
+    $.get((template.name + '.html'), function(temp) {
+      var htmlString = Mustache.render(temp, details)
+      $('#'+ template.name).html(htmlString)
+    })
+  }
 
-  // var loadSection = function(section){
-  //   loadTemplate(section.name, section.xtra)
-  //   for (i in section.items) {
-  //     var item = item[i] 
-  //     getTemplateString("item", item.content)
-  //   }
-  // }
+  var loadSection = function(section){
+    loadTemplate(section.name, section.xtra)
+    for (i in section.items) {
+      var item = item[i] 
+      getTemplateString("item", item.content)
+    }
+  }
   
-  // var toDisplay = {
-  //   RoR: false,
-  //   js: false
-  // }
+  var toDisplay = {
+    RoR: false,
+    js: false
+  }
 
-  // var sectionToggle = function(sections){
-  //   for (key in sections) {
-  //     if (sections.key.display === false){
-  //       $('#' + sections.key.name).fadeOut()
-  //     } else {
-  //       $('#' + sections.key.name).fadeIn()
-  //     }
-  //   }
-  // }
+  var sectionToggle = function(sections){
+    for (key in sections) {
+      console.log(sections[key].name)
+      if (sections[key].display === false){
+        $('#' + sections[key].name).fadeOut()
+      } else {
+        $('#' + sections[key].name).fadeIn()
+      }
+    }
+  }
 
-  // $('#portfolio-link')
-  //   .onclick(function(){
-  //     toDisplay = {
-  //       RoR: true,
-  //       js: true
-  //     }
-  //     sectionToggle(toDisplay)
-  //   })
-  //   $('#nodejs-link')
-  //     .onClick(function(){
-  //       if (toDisplay.js === true) {
-  //         toDisplay.js = false
-  //       } else {
-  //         toDisplay.js = true
-  //       }
-  //       if (toDisplay.RoR === true) {
-  //         toDiplay.RoR = false
-  //       } 
-  //       sectionToggle(toDisplay)
-  //     })
-  //   $('#RoR-link')
-  //     .onClick(function(){
-  //       if (toDisplay.RoR === true) {
-  //         toDisplay.RoR = false
-  //       } else {
-  //         toDisplay.RoR = true
-  //       }
-  //       if (toDisplay.js === true) {
-  //         toDiplay.js = false
-  //       } 
-  //       sectionToggle(toDisplay)
-  //     })
-  
+  $('#portfolio-link')
+    .click(function(){
+      toDisplay = {
+        RoR: true,
+        js: true
+      }
+      sectionToggle(toDisplay)
+    })
+  $('#nodejs-link').on('click', function(){
+      var display = sections.js.display
+      if (display === true) {
+        display = false
+      } else {
+        display = true
+      }
+      sections.js.display = display
+      console.log(sections.js.display)
+      sections.RoR.display = false
+      sectionToggle(sections)
+    })
+  $('#RoR-link')
+    .on('click', function(){
+      console.log("RoR Clicked")
+      var display = sections.RoR.display
+      console.log(display)
+      if (display === true) {
+        display = false
+      } else {
+        display = true
+      }
+      console.log(display)
+      sections.RoR.display = display 
+      sections.js.display = false
+      sectionToggle(sections)
+    })
+
 
 
 
